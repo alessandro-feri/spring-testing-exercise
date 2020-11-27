618aa37ab67a4517ad32a2ff04af44d783105724
alessandro <alessandro.feri@stud.unifi.it>
2020-11-27 15:49:19 +0100

    created project

---
+++ spring-testing-exercise/src/main/java/com/examples/spring/exercise/SpringTestingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.examples.spring.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestingExerciseApplication.class, args);
+	}
+
+}
---
+++ spring-testing-exercise/src/test/java/com/examples/spring/exercise/SpringTestingExerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.examples.spring.exercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTestingExerciseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
