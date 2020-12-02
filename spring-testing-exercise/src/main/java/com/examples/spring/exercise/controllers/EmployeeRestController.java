5584c9d9e6df1c4d4439575ebfa844e566ffce8c
alessandro <alessandro.feri@stud.unifi.it>
2020-12-02 16:16:54 +0100

    implementation of GET not empty

--- spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeRestController.java
+++ spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeRestController.java
@@ -1,18 +1,22 @@
 package com.examples.spring.exercise.controllers;
 
-import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examples.spring.exercise.model.Employee;
+import com.examples.spring.exercise.services.EmployeeService;
 
 @RestController
 public class EmployeeRestController {
+	
+	@Autowired
+	private EmployeeService employeeService;
 
 	@GetMapping("/api/employees")
 	public List<Employee> allEmployees() {
-		return Collections.emptyList();
+		return employeeService.getAllEmployees();
 	}
 } 
---
+++ spring-testing-exercise/src/main/java/com/examples/spring/exercise/services/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.examples.spring.exercise.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.examples.spring.exercise.model.Employee;
+
+/**
+ * 
+ * Temporary fake implementation of service
+ *
+ */
+
+@Service
+public class EmployeeService {
+
+	private static final String TEMPORARY_IMPLEMENTATION = "Temporary implementation";
+
+	public List<Employee> getAllEmployees() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+
+	}
+
+}
