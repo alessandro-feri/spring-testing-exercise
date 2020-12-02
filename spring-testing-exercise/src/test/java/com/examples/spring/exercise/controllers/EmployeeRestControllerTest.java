8f5f27ee028e9d5d2bff00792620433fa3d2dc26
alessandro <alessandro.feri@stud.unifi.it>
2020-12-02 16:13:21 +0100

    failing test for GET not empty

--- spring-testing-exercise/src/test/java/com/examples/spring/exercise/controllers/EmployeeRestControllerTest.java
+++ spring-testing-exercise/src/test/java/com/examples/spring/exercise/controllers/EmployeeRestControllerTest.java
@@ -1,16 +1,23 @@
 package com.examples.spring.exercise.controllers;
 
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.examples.spring.exercise.model.Employee;
+import com.examples.spring.exercise.services.EmployeeService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = EmployeeRestController.class)
@@ -18,6 +25,9 @@ public class EmployeeRestControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+	
+	@MockBean
+	private EmployeeService employeeService;
 
 	@Test
 	public void testAllEmployeesEmpty() throws Exception {
@@ -26,6 +36,24 @@ public class EmployeeRestControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(content().json("[]"));
 				// the above checks that the content is an empty JSON list
+	}
+	
+	@Test
+	public void testAllEmployeesNotEmpty() throws Exception {
+		when(employeeService.getAllEmployees()).
+			thenReturn(asList(
+				new Employee(1L, "first", 1000),
+				new Employee(2L, "second", 5000)
+			));
+		this.mvc.perform(get("/api/employees")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id", is(1)))
+				.andExpect(jsonPath("$[0].name", is("first")))
+				.andExpect(jsonPath("$[0].salary", is(1000)))
+				.andExpect(jsonPath("$[1].id", is(2)))
+				.andExpect(jsonPath("$[1].name", is("second")))
+				.andExpect(jsonPath("$[1].salary", is(5000)));
 	}
 
 } 
