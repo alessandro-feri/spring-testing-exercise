b78da3aee4f6122ab1c834a4783aafe08cb982ce
alessandro <alessandro.feri@stud.unifi.it>
2020-12-03 19:52:15 +0100

    implemented "message" attribute

--- spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeWebController.java
+++ spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeWebController.java
@@ -1,10 +1,13 @@
 package com.examples.spring.exercise.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.examples.spring.exercise.model.Employee;
 import com.examples.spring.exercise.services.EmployeeService;
 
 @Controller
@@ -15,7 +18,9 @@ public class EmployeeWebController {
 
 	@GetMapping("/")
 	public String index(Model model) {
-		model.addAttribute("employees", employeeService.getAllEmployees());
+		List<Employee> allEmployees = employeeService.getAllEmployees();
+		model.addAttribute("employees", allEmployees);
+		model.addAttribute("message", allEmployees.isEmpty() ? "No employee" : "");
 		
 		return "index";
 	}
