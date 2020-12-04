7d77e335ba67a2b95ccaaf7bad26916dfcab090e
alessandro <alessandro.feri@stud.unifi.it>
2020-12-04 10:39:21 +0100

    constants for model's attributes

--- spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeWebController.java
+++ spring-testing-exercise/src/main/java/com/examples/spring/exercise/controllers/EmployeeWebController.java
@@ -14,14 +14,18 @@ import com.examples.spring.exercise.services.EmployeeService;
 @Controller
 public class EmployeeWebController {
 	
+	private static final String MESSAGE_ATTRIBUTE = "message";
+	private static final String EMPLOYEE_ATTRIBUTE = "employee";
+	private static final String EMPLOYEES_ATTRIBUTE = "employees";
+	
 	@Autowired
 	private EmployeeService employeeService;
 
 	@GetMapping("/")
 	public String index(Model model) {
 		List<Employee> allEmployees = employeeService.getAllEmployees();
-		model.addAttribute("employees", allEmployees);
-		model.addAttribute("message", allEmployees.isEmpty() ? "No employee" : "");
+		model.addAttribute(EMPLOYEES_ATTRIBUTE, allEmployees);
+		model.addAttribute(MESSAGE_ATTRIBUTE, allEmployees.isEmpty() ? "No employee" : "");
 		
 		return "index";
 	}
@@ -29,8 +33,8 @@ public class EmployeeWebController {
 	@GetMapping("/edit/{id}")
 	public String editEmployee(@PathVariable long id, Model model) {
 		Employee employeeById = employeeService.getEmployeeById(id);
-		model.addAttribute("employee", employeeById);
-		model.addAttribute("message",
+		model.addAttribute(EMPLOYEE_ATTRIBUTE, employeeById);
+		model.addAttribute(MESSAGE_ATTRIBUTE,
 			employeeById == null ? "No employee found with id: " + id : "");
 		
 		return "edit";
@@ -38,8 +42,8 @@ public class EmployeeWebController {
 	
 	@GetMapping("/new")
 	public String newEmployee(Model model) {
-		model.addAttribute("employee", new Employee());
-		model.addAttribute("message", "");
+		model.addAttribute(EMPLOYEE_ATTRIBUTE, new Employee());
+		model.addAttribute(MESSAGE_ATTRIBUTE, "");
 		
 		return "edit";
 	}
